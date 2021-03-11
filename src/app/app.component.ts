7ac6cd753035de10c9782ec046d214546576dee0
rowenhama <tafadzwarhama@gmail.com>
2021-03-11 22:41:06 +0200

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,4 +27,24 @@ export class AppComponent implements OnInit{
     )
   }
 
+  public onOpenModal(employee:Employee,mode:string): void{
+    const button = document.createElement('button');
+    const container = document.getElementById('main-container');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle','modal');
+    if(mode==='add'){
+      button.setAttribute('data-target','#addEmployeeModal');
+    }
+    if(mode==='edit'){
+      button.setAttribute('data-target','#editEmployeeModal');
+    }
+    if(mode==='delete'){
+      button.setAttribute('data-target','#deleteEmployeeModal');
+    }
+
+    container?.appendChild(button);
+    button.click();
+  }
+
 }
